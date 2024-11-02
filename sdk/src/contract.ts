931bf00e132632b11cfe5ed23cde9178c835c138
asgarovf <fasgarov2002@gmail.com>
2024-11-02 21:23:06 +0300

    [sdk] init

---
+++ sdk/src/contract.ts
@@ -0,0 +1,67 @@
+import { getChainProvider } from './provider';
+import { ContractInterface } from 'ethers';
+import { Provider, Wallet, Contract } from 'zksync-ethers';
+import { InitOptions } from './types';
+
+type ContractName =
+    | 'batchCaller'
+    | 'implementation'
+    | 'registry'
+    | 'gaslessPaymaster'
+    | 'claveProxy'
+    | 'passkeyValidator'
+    | 'accountFactory';
+
+export const defaultContracts: Record<string, string> = {
+    batchCaller: '0x1513dB8DdC9420728bFb2830AE6784B26Ac9bf25',
+    implementation: '0x5627beD3bA7DFc5D9DbAa0122A52C7F22a2DD4D3',
+    registry: '0x7f273AF2576EA32309c32c9bae2b609B6e4484aC',
+    gaslessPaymaster: '0xF83F534153358AD6643B358AC3953f6467d5DAe7',
+    claveProxy: '0x3b633b071ABFf838d30D1a326744D8277Fad468c',
+    passkeyValidator: '0xDA63bBbc0A1a3F94e95c6bdd2DCB7B7112e3C635',
+    accountFactory: '0x281d01350B4449D6F4B3a58ce7F342c5221E1636',
+};
+
+export class SmartContract {
+    private readonly provider: Provider;
+    batchCaller: string;
+    implementation: string;
+    registry: string;
+    gaslessPaymaster: string;
+    claveProxy: string;
+    passkeyValidator: string;
+    accountFactory: string;
+
+    constructor({ contracts, chainId }: InitOptions) {
+        this.provider = getChainProvider(chainId);
+        this.batchCaller = contracts.batchCaller;
+        this.implementation = contracts.implementation;
+        this.registry = contracts.registry;
+        this.gaslessPaymaster = contracts.gaslessPaymaster;
+        this.claveProxy = contracts.claveProxy;
+        this.passkeyValidator = contracts.passkeyValidator;
+        this.accountFactory = contracts.accountFactory;
+    }
+
+    public static create(options: InitOptions): SmartContract {
+        return new SmartContract(options);
+    }
+
+    /**
+     * Getting a specific contract instance
+     */
+    public getContract(contractName: ContractName, abi: ContractInterface) {
+        return new Contract(this[contractName], abi, this.provider);
+    }
+
+    /**
+     * Getting a specific contract instance with EOA signer
+     */
+    public getContractWithEOASigner(
+        contractName: ContractName,
+        abi: ContractInterface,
+        wallet: Wallet,
+    ) {
+        return new Contract(this[contractName], abi, wallet);
+    }
+}
---
+++ sdk/src/signer.ts
@@ -0,0 +1,59 @@
+import { Webauthn } from './webauthn';
+import { BigNumber } from 'ethers';
+import { defaultAbiCoder } from 'ethers/lib/utils';
+
+export interface IPasskeySigner {
+    credentialId: string;
+    sign: (data: string) => Promise<string>;
+}
+
+export class Signer implements IPasskeySigner {
+    public readonly credentialId: string;
+
+    private readonly expectedClientDataPrefix = Webauthn.bufferFromString(
+        '{"type":"webauthn.get","challenge":"',
+    );
+
+    constructor(credentialId: string) {
+        this.credentialId = credentialId;
+    }
+
+    public async sign(data: string): Promise<string> {
+        const { response } = await Webauthn.authenticate(
+            [this.credentialId],
+            data,
+        );
+
+        const authenticatorDataBuffer = Webauthn.bufferFromBase64url(
+            response.authenticatorData,
+        );
+        const clientDataBuffer = Webauthn.bufferFromBase64url(
+            response.clientDataJSON,
+        );
+        const rs = Webauthn.getRS(response.signature);
+
+        return this.encodeSigature(
+            authenticatorDataBuffer,
+            clientDataBuffer,
+            rs,
+        );
+    }
+
+    private encodeSigature(
+        authenticatorData: Buffer,
+        clientData: Buffer,
+        rs: Array<BigNumber>,
+    ): string {
+        let clientDataSuffix = clientData
+            .subarray(this.expectedClientDataPrefix.length, clientData.length)
+            .toString();
+
+        const quoteIndex = clientDataSuffix.indexOf('"');
+        clientDataSuffix = clientDataSuffix.slice(quoteIndex);
+
+        return defaultAbiCoder.encode(
+            ['bytes', 'string', 'bytes32[2]'],
+            [authenticatorData, clientDataSuffix, rs],
+        );
+    }
+}
